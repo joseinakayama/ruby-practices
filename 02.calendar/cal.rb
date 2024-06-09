b871226ed5ca8f6f1ec698b8ee198bd7d2e391a1
josei <koga1.jyo.73@gmail.com>
2024-06-10 01:29:53 +0900

    インデントをspaceからtabに変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,10 +13,10 @@ this_month = Date.today.month
 this_year = Date.today.year
 
 if month == 0 && year == 0
-    month = this_month
-    year = this_year
+	month = this_month
+	year = this_year
 elsif year == 0
-    year = this_year
+	year = this_year
 end
 
 first_day = Date.new(year, month)
@@ -27,19 +27,19 @@ puts "#{space * 2} #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
 
 7.times do |i|
-    if first_day.cwday == i
-        print space * i
-    end
+	if first_day.cwday == i
+		print space * i
+	end
 end
 
 (first_day..last_day).each {|d|
-    if d.saturday? && d.day <= 9
-        print " #{d.day}\n"
-    elsif d.saturday?
-        print "#{d.day}\n"
-    elsif d.day <= 9
-        print " #{d.day} "
-    else
-        print "#{d.day} "
-    end
+	if d.saturday? && d.day <= 9
+		print " #{d.day}\n"
+	elsif d.saturday?
+		print "#{d.day}\n"
+	elsif d.day <= 9
+		print " #{d.day} "
+	else
+		print "#{d.day} "
+	end
 }
