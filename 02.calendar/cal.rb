a325b6735cafe26956e7d9ecc0640e19fc2aa419
josei <koga1.jyo.73@gmail.com>
2024-06-10 00:45:43 +0900

    input_date_year,input_date_month変数をyear,monthに変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,6 +2,7 @@
 
 require 'date'
 require 'optparse'
+
 opt = OptionParser.new
 
 opt.on('-m') 
@@ -9,26 +10,23 @@ opt.on('-y')
 
 input_date = ARGV.getopts('m:', 'y:')
 
-input_date_month = input_date['m'].to_i
-input_date_year = input_date['y'].to_i
-
+month = input_date['m'].to_i
+year = input_date['y'].to_i
 this_month = Date.today.month
 this_year = Date.today.year
 
-
-if input_date_month == 0 && input_date_year == 0
-    input_date_month = this_month
-    input_date_year = this_year
-elsif input_date_year == 0
-    input_date_year = this_year
+if month == 0 && year == 0
+    month = this_month
+    year = this_year
+elsif year == 0
+    year = this_year
 end
 
-
-first_day = Date.new(input_date_year, input_date_month)
-last_day = Date.new(input_date_year, input_date_month, -1)
+first_day = Date.new(year, month)
+last_day = Date.new(year, month, -1)
 space = "   "
 
-puts "#{space * 2} #{input_date_month}月 #{input_date_year}"
+puts "#{space * 2} #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
 case
 when first_day.cwday == 1
