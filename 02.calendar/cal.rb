c9eb6eb7135e19dc964fc123c6057853ce5bcaa1
josei <koga1.jyo.73@gmail.com>
2024-06-10 02:06:46 +0900

    rubocopの修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,7 +4,7 @@ require 'date'
 require 'optparse'
 
 opt = OptionParser.new
-opt.on('-m') 
+opt.on('-m')
 opt.on('-y')
 input_date = ARGV.getopts('m:', 'y:')
 month = input_date['m'].to_i
@@ -12,19 +12,19 @@ year = input_date['y'].to_i
 this_month = Date.today.month
 this_year = Date.today.year
 
-if month == 0 && year == 0
+if month.zero? && year.zero?
 	month = this_month
 	year = this_year
-elsif year == 0
+elsif year.zero?
 	year = this_year
 end
 
 first_day = Date.new(year, month)
 last_day = Date.new(year, month, -1)
-space = "   "
+space = '   '
 
 puts "#{space * 2} #{month}月 #{year}"
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 
 7.times do |i|
 	if first_day.cwday == i
