606d62949d991cd2ca06f125f3f9b712e18cda0e
josei <koga1.jyo.73@gmail.com>
2024-06-10 01:08:36 +0900

    case文をtimes文に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,12 +4,9 @@ require 'date'
 require 'optparse'
 
 opt = OptionParser.new
-
 opt.on('-m') 
 opt.on('-y')
-
 input_date = ARGV.getopts('m:', 'y:')
-
 month = input_date['m'].to_i
 year = input_date['y'].to_i
 this_month = Date.today.month
@@ -28,20 +25,26 @@ space = "   "
 
 puts "#{space * 2} #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
-case
-when first_day.cwday == 1
-    print space
-when first_day.cwday == 2
-    print space * 2
-when first_day.cwday == 3
-    print space * 3
-when first_day.cwday == 4
-    print space * 4
-when first_day.cwday == 5
-    print space * 5
-when first_day.cwday == 6
-    print space * 6
+
+7.times do |i|
+    if first_day.cwday == i
+        print space * i
+    end
 end
+# case
+# when first_day.cwday == 1
+#     print space
+# when first_day.cwday == 2
+#     print space * 2
+# when first_day.cwday == 3
+#     print space * 3
+# when first_day.cwday == 4
+#     print space * 4
+# when first_day.cwday == 5
+#     print space * 5
+# when first_day.cwday == 6
+#     print space * 6
+# end
 (first_day..last_day).each {|d|
     if d.saturday?
         if d.day <= 9
