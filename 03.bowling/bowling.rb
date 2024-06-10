462e0e7183ac1943f16f03c28671bfaf6347e131
josei <koga1.jyo.73@gmail.com>
2024-06-10 15:09:56 +0900

    robocop修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,35 +4,35 @@ score = 'X,0,0,X,0,0,X,0,0,X,0,0,X,0,0'
 scores = score.split(',')
 shots = []
 scores.each do |s|
-	if s == 'X'
-		shots << 10
-		shots << 0
-	else
-		shots << s.to_i
-	end
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
 end
 
 frames = []
 shots.each_slice(2) do |s|
-	frames << s
+  frames << s
 end
 
 point = 0
 frames.each_with_index do |frame, i|
-	if i < 9
-		if frame.first == 10
-			if frames[i+1].first == 10
-				point += frame.sum + frames[i+1].first + frames[i+2].first
-			else
-				point += frame.sum + frames[i+1].sum
-			end
-		elsif frame.sum == 10
-			point += frame.sum + frames[i+1][0]
-		else
-			point += frame.sum
-		end
-	else
-		point += frame.sum
-	end
+  if i < 9
+    if frame.first == 10
+      point = if frames[i + 1].first == 10
+                frame.sum + frames[i + 1].first + frames[i + 2].first
+              else
+                frame.sum + frames[i + 1].sum
+              end
+    elsif frame.sum == 10
+      point += frame.sum + frames[i + 1][0]
+    else
+      point += frame.sum
+    end
+  else
+    point += frame.sum
+  end
 end
 puts point
